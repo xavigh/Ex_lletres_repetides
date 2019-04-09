78196f28e2ded459553924363ff77a7a94e69877
xaviGH <fgargallo@gmail.com>
2019-04-09 20:30:59 +0200

    Fase 1

---
+++ js/basic.js
@@ -0,0 +1,12 @@
+//exercici lletres repetides
+//Fase 1
+
+function nameLetters(){
+    var name = ['X','a','v','i'];
+    var i = 0;
+    var txt;
+    while( i < name.length){
+       txt = console.log(name[i]);
+      i++;
+    }
+}
