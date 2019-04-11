e0ca22e51209c35f29f20edb1aad6a4c045035d4
xavigk <fgargallo@gmail.com>
2019-04-11 11:28:18 +0200

    Fase 3, ejercicio repeticiones
    new map object with has get and set methods

--- js/js2.js
+++ js/js2.js
@@ -9,26 +9,19 @@ function nameLetters(){
 
 
     //loop and findout if vocal, consonant or number.
-    for(i=0; i < name.length; i++){
+    for(i=0; i < name.length; i++){// first we check if it is a digit or a letter
 
-        // first we check if it is a digit or a letter
-        if( isNaN(name[i]) ){
-            //if yes it is a number then check if it is a vocal
+        if( isNaN(name[i]) ){  //if yes it is a number then check if it is a vocal
               var str="aeiou_AEIOU";
-
               if(  str.match(name[i]) ){
                 console.log("this is a vocal -->"+ name[i]);
               }
               else {// not a vocal, not a number then it is a consonant
                 console.log("it is a consonant -->"+ name[i]);
               }
-        }else {// if it is a number get the number
-          console.log("it is a number -->"+ name[i]);
-        }
-
-
-        // if itś a digit then we input the digit.
-    }
-
-
+              }
+        else {// if it is a number get the number
+                console.log("it is a number -->"+ name[i]);
+              }
+    }//for loop // if itś a digit then we input the digit.
 }// end of function
--- js/js3.js
+++ js/js3.js
@@ -4,12 +4,16 @@
 // then count
 //Fase 3
 function arrMapCount(){
-    var name = ['A','L','E','J','A','N','D','R','O'];
-    var sort_arr = name.sort();
-        console.log(name);
-    var i = 0;
-
-
-
-
+    var arrName = ['A','L','E','J','A','N','D','R','O'];
+    //map function to count repetitions and get  each item of the array
+        var arr_letters = new Map();
+        for(i=0; i < arrName.length; i++){
+            if(arr_letters.has(arrName[i])){
+              arr_letters.set(arrName[i], arr_letters.get(arrName[i])+1);
+            }
+            else{
+              arr_letters.set(arrName[i], 1);
+          }
+    } //for loop
+console.log(arr_letters);
 }// end of function
